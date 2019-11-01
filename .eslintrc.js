c3ffefe6a0345c74b25790256ad4ed1983981d89
ZhengXuJiang <zhengxujiang@jiayouxueba.cn>
2019-11-01 14:36:32 +0800

    chore: 优化配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
   parser: 'babel-eslint',
-  extends: ['airbnb', 'prettier', 'prettier/react'],
-  plugins: [
-    'prettier',
-    'react',
-    'react-hooks',
-    'jsx-a11y',
-    'compat/recommended',
-    'jest/recommended',
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'plugin:jest/recommended',
   ],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'jest', 'eslint-comments', 'prettier'],
   env: {
     browser: true,
     node: true,
@@ -28,59 +29,60 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'prettier/prettier': ['error', prettierOptions],
-    'arrow-body-style': [2, 'as-needed'],
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'no-console': 0,
-    'no-param-reassign': ['error', { props: false }],
-    'import/prefer-default-export': 0,
-    'react/jsx-filename-extension': 0,
+    'prettier/prettier': 2,
     'react/jsx-wrap-multilines': 0,
     'react/prop-types': 0,
     'react/forbid-prop-types': 0,
     'react/jsx-one-expression-per-line': 0,
+    'generator-star-spacing': 0,
+    'function-paren-newline': 0,
     'import/no-unresolved': [2, { ignore: ['^@/'] }],
-    'import/no-cycle': 0,
+    'import/order': 'warn',
+    'import/no-extraneous-dependencies': [
+      2,
+      {
+        optionalDependencies: true,
+        devDependencies: [
+          '**/tests/**.{ts,js,jsx,tsx}',
+          '**/_test_/**.{ts,js,jsx,tsx}',
+          '/mock/**/**.{ts,js,jsx,tsx}',
+          '**/**.test.{ts,js,jsx,tsx}',
+          '**/_mock.{ts,js,jsx,tsx}',
+          '**/example/**.{ts,js,jsx,tsx}',
+          '**/examples/**.{ts,js,jsx,tsx}',
+        ],
+      },
+    ],
     'jsx-a11y/no-noninteractive-element-interactions': 0,
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/no-static-element-interactions': 0,
     'jsx-a11y/anchor-is-valid': 0,
     'linebreak-style': 0,
+    // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
+    'no-prototype-builtins': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': [0, 'camel-case'],
+    // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
+    'react/destructuring-assignment': 'off',
+    'react/jsx-filename-extension': 'off',
+    // Use function hoisting to improve code readability
+    'no-use-before-define': ['error', { functions: false, classes: true, variables: true }],
+    // Common abbreviations are known and readable
+    'import/no-cycle': 0,
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    // issue https://github.com/facebook/react/issues/15204
+    'react-hooks/exhaustive-deps': 'off', // Checks effect dependencies
+    // Conflict with prettier
+    'arrow-body-style': ['error', 'as-needed'],
+    'arrow-parens': 1,
+    'object-curly-newline': 0,
+    'implicit-arrow-linebreak': 0,
+    'operator-linebreak': 0,
+    'eslint-comments/no-unlimited-disable': 1,
+    'no-param-reassign': 1,
+    'space-before-function-paren': 0,
   },
-  overrides: [
-    {
-      files: ['*.test.js', '*.test.ts', '*.test.jsx', '*.test.tsx'],
-      env: {
-        jest: true, // now **/*.test.js files' env has both es6 *and* jest
-      },
-      plugins: ['jest'],
-      rules: {
-        'jest/no-disabled-tests': 'warn',
-        'jest/no-focused-tests': 'error',
-        'jest/no-identical-title': 'error',
-        'jest/prefer-to-have-length': 'warn',
-        'jest/valid-expect': 'error',
-      },
-    },
-    {
-      files: ['*.ts', '*.tsx'],
-      parser: '@typescript-eslint/parser',
-      plugins: ['@typescript-eslint'],
-      rules: {
-        'no-var-requires': 0,
-        'no-submodule-imports': 0,
-        'object-literal-sort-keys': 0,
-        'jsx-no-lambda': 0,
-        'no-implicit-dependencies': 0,
-        'no-console': 0,
-        'order-import': 0,
-        'no-use-before-define': 0,
-        '@typescript-eslint/no-unused-vars': [2, { args: 'none' }],
-        '@typescript-eslint/no-use-before-define': 0,
-      },
-    },
-  ],
   settings: {
-    polyfills: ['fetch', 'promises', 'url', 'object-assign'],
+    polyfills: ['fetch', 'Promise', 'URL', 'object-assign'],
   },
 };
